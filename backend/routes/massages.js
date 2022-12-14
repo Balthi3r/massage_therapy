39911ba64832c7b4e2e7bc2b3a36327069353a76
balthier <rrodrijose@gmail.com>
2022-12-14 17:54:26 -0600

    fetchs/deletes

--- backend/routes/massages.js
+++ backend/routes/massages.js
@@ -13,20 +13,48 @@ router.post("/",async (req,res)=>{
     }catch(err){
         res.status(500).json(err)
     }
-})
+});
 //update
 router.put("/:id",async (req,res)=>{
     const updateMassages= new massages (req.body)
     try{
-        const updateMassages= await Massages.findByIDAndUpdate(req.params.id, {$set: req.body})
+       await Massages.findByIDAndUpdate(req.params.id, {$set: req.body})
         res.status(200).json(updatemassage)
 
     }catch(err){
         res.status(500).json(err)
     }
-})
+});
 
 //delete
+router.delete("/:id",async (req,res)=>{
+    try{
+       await Massages.findByIDAndDelete(req.params.id);
+        res.status(200).json("massage is no longer being done")
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
 
+//get 
+router.delete("/:id",async (req,res)=>{
+    try{
+       await Massages.findByID(req.params.id);
+        res.status(200).json(massage)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
 
-export default router
+//get all
+ 
+router.get("/",async (req,res)=>{
+    try{
+       await Massages.find(req.params.id);
+        res.status(200).json(massage)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+export default router;
