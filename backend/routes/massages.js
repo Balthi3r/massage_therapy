1b941f9cf0802e830bb1dc330f186dcdb8066ea0
balthier <rrodrijose@gmail.com>
2022-12-14 18:47:49 -0600

    massagecontrol cleanup

--- backend/routes/massages.js
+++ backend/routes/massages.js
@@ -1,60 +1,19 @@
 import  express from "express";
+import { deleteMassage, getAllMassages, getMassage } from "../controllers/massage";
 import massages from "../models/massages"
 
 const router =express.router();
 
 //create
-router.post("/",async (req,res)=>{
-    const newMassages= new massages (req.body)
-    try{
-        const savedMassages= await newMassages.save()
-        res.status(200).json(savedmassage)
-
-    }catch(err){
-        res.status(500).json(err)
-    }
-});
+router.post("/",createMassage);
 //update
-router.put("/:id",async (req,res)=>{
-    const updateMassages= new massages (req.body)
-    try{
-       await Massages.findByIDAndUpdate(req.params.id, {$set: req.body})
-        res.status(200).json(updatemassage)
-
-    }catch(err){
-        res.status(500).json(err)
-    }
-});
-
+router.put("/:id",updateMassage);
 //delete
-router.delete("/:id",async (req,res)=>{
-    try{
-       await Massages.findByIDAndDelete(req.params.id);
-        res.status(200).json("massage is no longer being done")
-    }catch(err){
-        res.status(500).json(err)
-    }
-});
-
+router.delete("/:id",deleteMassage);
 //get 
-router.delete("/:id",async (req,res)=>{
-    try{
-       await Massages.findByID(req.params.id);
-        res.status(200).json(massage)
-    }catch(err){
-        res.status(500).json(err)
-    }
-});
-
+router.delete("/:id",getMassage);
 //get all
- 
-router.get("/",async (req,res)=>{
-    try{
-       await Massages.find(req.params.id);
-        res.status(200).json(massage)
-    }catch(err){
-        res.status(500).json(err)
-    }
-});
+router.get("/",getAllMassages);
+
 
 export default router;
