6e1936b15eaba701932c02ebe4a370861a6a8f33
balthier <rrodrijose@gmail.com>
2022-12-17 21:54:45 -0600

    reinstalls

--- backend/index.js
+++ backend/index.js
@@ -18,12 +18,26 @@ const connect= async() =>{
 };
 
 //middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error));
+
+
 app.use(cookieParser());
 app.use("/auth", authRoute)
 app.use("/users", usersRoute)
 app.use("/massages", massagesRoute)
 
-app.listen(3000,()=>{
-    connect()
-    console.log("connected0")
-})
